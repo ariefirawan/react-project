d03480199319053f9b36ccceaeb8802dcbfa1a4e
ariefirawan <ariefirawan@users.noreply.github.com>
2020-03-08 13:37:18 +0700

    -remove item from cartItems

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -2,9 +2,14 @@ import { cartActionTypes } from './cart.types';
 
 export const toggleCartHidden = () => ({
   type: cartActionTypes.TOGGLE_CART_HIDDEN
-})
+});
 
 export const addItem = item => ({
   type: cartActionTypes.ADD_ITEM,
   payload: item
-})
+});
+
+export const clearItemFromCart = item => ({
+  type: cartActionTypes.CLEAR_CART_ITEM,
+  payload: item
+});
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { cartActionTypes } from './cart.types';
-import {addItemToCart} from './cart.utils';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -17,7 +17,14 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
-      }
+      };
+    case cartActionTypes.CLEAR_CART_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      };
     default:
       return state;
   }
