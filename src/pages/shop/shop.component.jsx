fede44713a3a1df0407b9104dbc553871a78aed3
ariefirawan <ariefirawan@users.noreply.github.com>
2020-03-09 11:16:57 +0700

    gg route

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import { selectShopCollections } from '../../redux/shop/shop.selector';
-
-import PreviewCollection from '../../components/preview-collection/preview-collection.component';
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
 
 import './shop.styles.scss';
 
 const ShopPage = ({ collections }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <PreviewCollection key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
