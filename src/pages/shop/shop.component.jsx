f963d40f717c913c5c060bdc5090895e09a29958
ariefirawan <ariefirawan@users.noreply.github.com>
2020-03-10 11:49:46 +0700

    converts or normalizes data array into an object or what we call a hash table

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -8,7 +8,7 @@ import "./shop.styles.scss";
 
 const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <Route exact path={`{match.path}`} component={CollectionOverview} />
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
     <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
